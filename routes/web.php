f21617d84c0d244127931bde93929bac39cdcd1d
Jamewell <jamiew746@gmail.com>
2025-04-25 02:11:56 +0200

    Add throttle middleware to password creation route

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\PasswordShareController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PasswordShareController::class, 'create'])
-    ->name('password.create');
+    ->name('password.create')
+    ->middleware('throttle:10,1');
 
 Route::get('/password/{id}', [PasswordShareController::class, 'show'])
     ->name('password.show');
